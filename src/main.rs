cf04a81408f45c98cba7e539f386fd607491b27c
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2016-12-03 18:15:22 -0500

    Improve again.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{thread, time};
 
 fn main() {
     let now = time::Instant::now();
-    let dur = time::Duration::from_millis(15000);
+    let dur = time::Duration::from_millis(10000);
     thread::sleep(dur);
     println!("dur: {}.{:06}",
              now.elapsed().as_secs(),
