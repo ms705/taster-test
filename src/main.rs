e347a27b95720d56b4e0ae6bf85f78203e02f403
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2017-11-14 14:02:20 -0500

    Slow down

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::{thread, time};
 
 fn main() {
     let now = time::Instant::now();
-    let dur = time::Duration::from_millis(3000);
+    let dur = time::Duration::from_millis(30000);
     thread::sleep(dur);
     println!("dur: {}.{:06}",
              now.elapsed().as_secs(),
